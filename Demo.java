f6d34cf5cfd4068d4d602f8378443aa68b084331
Justin_May <e135527@rrisd.local>
2019-11-12 09:00:34 -0600

    adding java file for demo

---
+++ Demo.java
@@ -0,0 +1,19 @@
+public class Demo
+{
+  public void run()
+  {
+    MrMay();
+  }
+
+  public void MrMay()
+  {
+    System.out.println("Computer Science!");
+  }
+
+  
+  public static void main(String[] args)
+  {
+    Demo d = new Demo();
+    d.run();
+  }
+}
