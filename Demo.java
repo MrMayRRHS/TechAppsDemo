986ec55899505cc42cfe00e72c2f9df3091dce78
White.Kevin.s651670 <s651670@rrisd.local>
2019-11-20 11:35:19 -0600

    plz work

--- Demo.java
+++ Demo.java
@@ -1,28 +1,12 @@
-<<<<<<< HEAD
-public class Demo
-{
-  public void run()
-  {
-    MrMay();
 
-    Kevin();
-
-    JacksonWelsh();
-
-
-    MilesCrain();
-  }
-  g=
-=======
->>>>>>> a86a9b75a8a91f5a0e14d36262ea25177eaa00f6
 public class Demo {
     public void run() {
         MrMay();
         JacksonWelsh();
         MilesCrain();
+        KevinWhite();
     }
 
-<<<<<<< HEAD
     public void MrMay() {
         System.out.println("bing bing");
     }
@@ -32,39 +16,31 @@ public class Demo {
     Demo d = new Demo();
     d.run();
   }
-  public void MilesCrain(){
-System.out.println("SHANTANU IS BAD AT CHESS");
-=======
 
-    void MrMay() {
-        System.out.println("bing bing");
+
+    void MrMay () {
+      System.out.println("bing bing");
     }
 
 
-    void JacksonWelsh() {
-        for (int i = Integer.MIN_VALUE; i < Integer.MAX_VALUE; i++)
-            System.out.println("i'm stuff" + i);
+    void JacksonWelsh () {
+      for (int i = Integer.MIN_VALUE; i < Integer.MAX_VALUE; i++)
+        System.out.println("i'm stuff" + i);
     }
 
 
-    public static void main(String[] args) {
-        Demo d = new Demo();
-        d.run();
+
+
+    void MilesCrain () {
+      System.out.println("SHANTANU IS BAD AT CHESS");
     }
-
-    void MilesCrain() {
-        System.out.println("SHANTANU IS BAD AT CHESS");
->>>>>>> a86a9b75a8a91f5a0e14d36262ea25177eaa00f6
-
-public void Kevin();
+    public void KevinWhite();
+    {
+      System.out.println("PMA");
+    }
+  public static void main (String[]args)
   {
-    System.out.println("PMA");
+    Demo d = new Demo();
+    d.run();
   }
-  
-    public static void main(String[] args) {
-        Demo d = new Demo();
-        d.run();
-    }
-
-    }
-}
+  }
